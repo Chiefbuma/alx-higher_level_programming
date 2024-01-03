4c9668a4c9c5e2b28fe136dc7463862d8902a8c1
Chiefbuma <jorgebuma.jb@gmail.com>
2024-01-03 12:22:53 -0800

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if j!= 99:
-        print("{:02d}, ".format(j), end='')
-    else:
-        print("{:02d}".format(j))
+for i in range(0, 99):
+    print("{:02d}".format(i), end=', ')
+print("{:02d}".format(i + 1))
