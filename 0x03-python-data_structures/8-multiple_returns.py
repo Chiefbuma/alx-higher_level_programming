194faa4ccfba184ee46e249772a3f9a66cc081e9
Chiefbuma <122814097+Chiefbuma@users.noreply.github.com>
2024-01-07 11:49:22 -0800

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 
 
 def multiple_returns(sentence):
-    """Returns the length of a string and its first character."""
-    if sentence == "":
+    """Sentence length."""
+    if sent == "":
         return (0, None)
-    return (len(sentence), sentence[0])
+    return (len(sent), sent0])
