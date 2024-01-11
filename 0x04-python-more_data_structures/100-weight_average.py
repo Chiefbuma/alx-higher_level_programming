839004a60ba48be10b242573e1c543da6802eb27
Chiefbuma <122814097+Chiefbuma@users.noreply.github.com>
2024-01-11 22:22:07 +0300

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if my_list and len(my_list):
-        num = 0
-        dem = 0
+        nmn = 0
+        kml = 0
         for tup in my_list:
-            num += (tup[0] * tup[1])
-            dem += tup[1]
-        return (num / dem)
+            nmn += (tup[0] * tup[1])
+            kml += tup[1]
+        return (nmn / kml)
     return 0
