1b3e6dbb5c0e37e1ff128d5b84ee1cf488eb229f
Chiefbuma <122814097+Chiefbuma@users.noreply.github.com>
2024-02-11 15:42:06 +0300

    3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Contains class function
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """True if obj is  from a_class, else False"""
+    return (isinstance(obj, a_class))
